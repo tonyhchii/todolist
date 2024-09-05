6f55a26f492c548fd735ec68d002c5350c697e9c
Tony Huang <tonyhchii@gmail.com>
2024-09-05 13:09:57 -0400

    fixed webpack.dev file and added todo object

---
+++ src/todoItem.js
@@ -0,0 +1,20 @@
+export class todoItem {
+    constructor (title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.completed = false;
+    }
+
+    toggleCompleted() {
+        this.completed = !this.completed;
+    }
+
+    setTodo(title,description,dueDate,priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
