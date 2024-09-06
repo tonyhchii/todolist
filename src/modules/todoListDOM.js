2b8e60e8c87e295d6e0d25d48be584c86a4eefbf
Tony Huang <tonyhchii@gmail.com>
2024-09-06 11:23:15 -0400

    refactored todoListDOM

--- src/modules/todoListDOM.js
+++ src/modules/todoListDOM.js
@@ -9,30 +9,44 @@ project1.addToDo('Button','Add a button that adds new ToDos','3','4');
 
 export function displayToDo () {
 
+    displayHeader();
+    displayBody();
+
+}
+
+
+function displayHeader() {
+    todoHeader.innerHTML = "";
     const projHeader = document.createElement('h1');
     projHeader.textContent = project1.name;
     todoHeader.appendChild(projHeader);
+}
 
+function displayBody() {
+    todoBody.innerHTML = "";
     project1.todoList.forEach(todo => {
-        const card = document.createElement('div');
-        card.classList.add('card');
-        todoBody.appendChild(card);
-
-        const cardTitle = document.createElement('h3');
-        cardTitle.textContent = todo.title;
-        card.appendChild(cardTitle);
-        
-        const cardDes = document.createElement('p') 
-        cardDes.textContent = todo.description;
-        card.appendChild(cardDes);
-
-        const cardEdit = document.createElement('button');
-        cardEdit.textContent = 'Edit';
-        card.appendChild(cardEdit);
-
-        const cardDelete = document.createElement('button');
-        cardDelete.textContent = 'Delete';
-        card.appendChild(cardDelete);
+        createCard(todo);
     })
-   
+}
+
+function createCard(todo) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    todoBody.appendChild(card);
+
+    const cardTitle = document.createElement('h3');
+    cardTitle.textContent = todo.title;
+    card.appendChild(cardTitle);
+    
+    const cardDes = document.createElement('p') 
+    cardDes.textContent = todo.description;
+    card.appendChild(cardDes);
+
+    const cardEdit = document.createElement('button');
+    cardEdit.textContent = 'Edit';
+    card.appendChild(cardEdit);
+
+    const cardDelete = document.createElement('button');
+    cardDelete.textContent = 'Delete';
+    card.appendChild(cardDelete);
 }
