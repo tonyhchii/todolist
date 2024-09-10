fcc07613e503842b8cb7dc1cb3de86e13a40c016
Tony Huang <tonyhchii@gmail.com>
2024-09-10 16:53:01 -0400

    added task adder and tasks

--- src/modules/controller.js
+++ src/modules/controller.js
@@ -5,6 +5,13 @@ const listsContainer = document.querySelector('[data-lists]');
 const newListForm = document.querySelector('[data-new-list-form]');
 const newListInput = document.querySelector('[data-new-list-input]');
 
+const listDisplayContainer = document.querySelector('[data-list-display-container]');
+const listTitleElement = document.querySelector('[data-list-title]');
+const tasksContainer = document.querySelector('[data-tasks]');
+const taskTemplate = document.getElementById('task-template');
+const newTaskForm = document.querySelector('[data-new-task-form');
+const newTaskInput = document.querySelector('[data-new-task-input');
+
 const LOCAL_STORAGE_LIST_KEY = 'task.lists';
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
 
@@ -21,20 +28,44 @@ newListForm.addEventListener('submit', e => {
     saveAndRender()
 })
 
+newTaskForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const taskName = newTaskInput.value;
+    if (taskName == null || taskName === '') return;
+    const task = createTask(taskName)
+    newTaskInput.value = null;
+    const selectedList = lists.find(list => list.id === selectedListId)
+    selectedList.tasks.push(task)
+    saveAndRender()
+})
+
+tasksContainer.addEventListener('click', e => {
+    if(e.target.tagName.toLowerCase() === 'input'){
+        const selectedList = lists.find(list => list.id === selectedListId);
+        const selectedTask = selectedList.tasks.find(task => task.id === e.target.id);
+        selectedTask.complete = e.target.checked;
+        save();
+    }
+})
+
 listsContainer.addEventListener('click', e => {
     if(e.target.tagName.toLowerCase() === 'li') {
         selectedListId = e.target.dataset.listId;
         saveAndRender();
     }
     if(e.target.tagName.toLowerCase() === 'img') {
-        deleteList(e.target.id);
+        const targetID = e.target.id;
+        deleteList(targetID);
     }
 })
 
 function createList(name) {
-    return { id: Date.now().toString(), name: name, tasks:[] } 
+    return { id: Date.now().toString(), name: name, tasks:[] };
 }
 
+function createTask(name) {
+    return { id: Date.now().toString(), name: name, complete: false };
+}
 export function saveAndRender(){
     save();
     render();
@@ -45,8 +76,23 @@ function save() {
     localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
 }
 
-function render() {
+export function render() {
     clearElement(listsContainer);
+    renderLists();
+
+    const selectedList = lists.find(list => list.id === selectedListId);
+
+    if (!selectedList) {
+        listDisplayContainer.style.display = 'none'
+    } else {
+        listDisplayContainer.style.display = ''
+        listTitleElement.innerText = selectedList.name
+        clearElement(tasksContainer)
+        renderTasks(selectedList)
+    }
+}
+
+function renderLists() {
     lists.forEach(list => {
         const listElement = document.createElement("li");
         listElement.dataset.listId = list.id;
@@ -67,6 +113,19 @@ function render() {
         listElement.appendChild(removeButton);
 
         listsContainer.appendChild(listElement);
+    });
+}
+
+function renderTasks(selectedList) {
+    selectedList.tasks.forEach(task => {
+        const taskElement = document.importNode(taskTemplate.content, true);
+        const checkbox = taskElement.querySelector('input');
+        checkbox.id = task.id;
+        checkbox.checked = task.complete
+        const label = taskElement.querySelector('label');
+        label.htmlFor = task.id;
+        label.append(task.name);
+        tasksContainer.appendChild(taskElement);
     })
 }
 
@@ -78,5 +137,18 @@ function clearElement(element) {
 
 function deleteList (buttonID) {
     lists = lists.filter(list => list.id !== buttonID);
+    if (buttonID == selectedListId) {
+        selectedListId = null;
+    }
     saveAndRender();
+}
+
+export function getListID() {
+    console.log('selectedListId = ' + selectedListId);
+}
+
+export function getLists() {
+    lists.forEach(list => {
+        console.log(list.name + list.id);
+    })
 }
